261871bbaad3c2e975d492dfa2fe7df4417a5eb8
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-22 21:05:21 +0800

    print divisible by 5

--- print_divisible_by_five.py
+++ print_divisible_by_five.py
@@ -1,3 +1,11 @@
 # Exercise 6: Display numbers divisible by 5 from a list
 # Iterate the given list of numbers 
 # print only those numbers which are divisible by 5
+
+num_list = [5, 23, 35, 53, 75, 95, 18, 100]
+print ("Given lists", num_list)
+print("Divisible by 5: ")
+for num in num_list:
+    if num % 5 == 0:
+        print(num)
+
